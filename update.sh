c7a0900eeff0e6958b28b35356ed1d994988e954
Matthew B. Gray <debug@matthew.nz>
2023-09-12 09:02:39 +1200

    feat: Inline update script for simplicity

--- update.sh
+++ update.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 
-# Define the platform function
-platform() {
-  case "$(uname -s)" in
-    Darwin*) echo "darwin" ;;
-    *) echo "unknown" ;;
-  esac
-}
+echo "vim..."
+vim -c "call execute('PlugUpdate') | call execute('GoUpdateBinaries') | qa"
 
-# Run script for specified platform
-SCRIPT_DIR=$(dirname -- "${BASH_SOURCE[0]}")
-source "$SCRIPT_DIR/update/$(platform).sh"
+echo "npm..."
+npm install -g npm # update npm itself
+npm update -g      # update global packages
+
+echo "julia..."
+julia -e 'using Pkg; Pkg.update()'
+
+echo "brew..."
+brew upgrade
+brew upgrade --cask
+brew cleanup
+brew doctor
+
+echo "macOS..."
+echo "This command appears to freeze:"
+echo "> softwareupdate --install --all"
+echo "...try updating manually instead"
+# system('sudo softwareupdate --install --all')
