25297d276ec44ca903c089f9b93550db8f6f2ba6
Matthew B. Gray <debug@matthew.nz>
2023-09-22 15:39:12 +1200

    feat: Update prints out current process for vim
    Just means I don't just imagine it's locked up

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "vim..."
-vim -c "call execute('PlugUpdate') | call execute('GoUpdateBinaries') | qa"
+vim -c 'call execute("PlugUpdate") | echo "Go Update..." | call execute("GoUpdateBinaries") | qa'
 
 echo "npm..."
 npm install -g npm # update npm itself
