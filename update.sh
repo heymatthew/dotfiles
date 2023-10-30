d6d681ec0f28c5f08bbf1ff0d23faba947a360ed
Matthew B. Gray <debug@matthew.nz>
2023-10-31 12:57:34 +1300

    feat: Rotate aws keys on update

--- update.sh
+++ update.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if which aws-vault > /dev/null && [ -n "$AWS_PROFILE" ]; then
+  echo "rotating aws keys..."
+  aws-vault rotate "$AWS_PROFILE"
+fi
+
 echo "vim..."
 vim -c 'call execute("PlugUpdate") | echo "Go Update..." | call execute("GoUpdateBinaries") | qa'
 
