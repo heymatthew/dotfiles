0af73285e167d50ee41cea2d2b8fd77d87529dd8
Matthew B. Gray <debug@matthew.nz>
2024-04-08 20:54:23 +1200

    feat: Convert neovim back to vimscript
    It's just a bit more concise than lua. I might come back to this later.

--- softlinks/.config/nvim/init.lua
+++
@@ -1,3 +0,0 @@
--- Quick iteration on nvim config, credit http://howivim.com/2016/damian-conway
-vim.keymap.set('n', '<leader>v', ':edit $MYVIMRC<CR>', { noremap = true, silent = true })
-vim.cmd "autocmd BufWritePost $MYVIMRC source $MYVIMRC"
