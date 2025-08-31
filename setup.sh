84f8861fd682d6c82726d818ad971b00e6ed5c50
Matthew <debug@matthew.nz>
2025-09-01 10:12:00 +1200

    fix: Don't stow entire ~/.config, only track some things

--- setup.sh
+++ setup.sh
@@ -59,6 +59,7 @@ echo "removing .DS_Store files in softlinks"
 find softlinks -name ".DS_Store" -depth -exec rm -f {} \;
 
 echo "stowing dotfiles..."
+mkdir ~/.config
 /opt/homebrew/bin/stow -vv softlinks
 
 echo "source updated zshrc..."
